a24c0405b4d5460b85e6fcd99a793f79c1415883
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-12-29 13:21:51 +0800

    修改

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+// 何意味?
+
 const moneyOwnedDOM = document.getElementById("moneyOwned");
 const dayDOM = document.getElementById("day");
 
@@ -5,6 +7,9 @@ const totalNetOwnedDOM = document.getElementById("totalNetOwned");
 const netInshoreDOM = document.getElementById("netInshore");
 const netOutshoreDOM = document.getElementById("netOutshore");
 const netExtraDOM = document.getElementById("netExtra");
+
+const netBuyNumberDOM = document.getElementById("netBuyNumber");
+const moneyCostDOM = document.getElementById("moneyCost");
 
 const weatherDOM = document.getElementById("weather");
 
@@ -20,6 +25,12 @@ const netPriceDOM = document.getElementById("netPrice");
 
 const rentMoneyDOM = document.getElementById("rentMoney");
 
+const resultBoxDOM = document.getElementById("resultBox");
+
+const netDamagedTextDOM = document.getElementById("netDamagedText");
+
+const moneyProfitDOM = document.getElementById("moneyProfit");
+
 let moneyOwned = 50;
 let day = 1;
 
@@ -30,24 +41,26 @@ let net = {
 }
 
 let weatherIsGood = null;
-let badWeatherPosibility = 0.4;
+let badWeatherPosibility = 0.5;
 
 let netIncomePosibility = {
     inshore: {
         minimum: 2,
-        maximum: 8
+        maximum: 6
     },
     outshore: {
-        minimum: 8,
-        maximum: 16
+        minimum: 6,
+        maximum: 12
     }
 }
 
 let netPrice = null;
 let netPricePosibility = {
-    minimum: 2,
+    minimum: 5,
     maximum: 30
 }
+
+let netBuyNumber = 0;
 
 let incomePrice = {
     inshore: 0,
@@ -57,6 +70,10 @@ let incomePrice = {
 let rentMoney = 80;
 
 let manuOpen = false;
+
+let inRound = false;
+
+let isInWeekEnd = false;
 
 function setWeatherCondition() {
     let randomNumber = Math.random();
@@ -93,16 +110,13 @@ function setWeather() {
     if (weatherIsGood) {
         weatherDOM.innerHTML = "好天气";
         weatherDOM.className = "greenText";
-    } else if (weatherIsGood = null) {
-        weatherDOM.innerHTML = "未知";
-        weatherDOM.className = undefined;
     } else {
         weatherDOM.innerHTML = "坏天气";
         weatherDOM.className = "redText";
     }
 }
 
-function checkBuyManu() {
+function buyButtonPressed() {
     buyButtonDOM.style = "animation: 1s ease-out shiftFromRightToLeft; transform: translate(-100%, -50%);";
     buyManuDOM.style = "animation: 1s ease-out shiftFromLeftToRight; transform: translate(0, -50%);";
 
@@ -118,13 +132,160 @@ function setIncomePriceDOM() {
     rentMoneyDOM.innerHTML = rentMoney + '$';
 }
 
-function setNextButtonDOM(type) {
-    if (type == "nextDay") {
-        nextButtonDOM.innerHTML = "下一天";
-    } else if (type == "start") {
+function nextButtonPressed() {
+    if (inRound) {
+        inRound = false;
+
+        weatherDOM.innerHTML = "null";
+        weatherDOM.className = "";
+
         nextButtonDOM.innerHTML = "开始";
+        resultBoxDOM.hidden = true;
+
+        netDamagedTextDOM.hidden = true;
     } else {
-        throw new Error("o.0");
+        inRound = true;
+
+        let moneyProfit = 0;
+
+        nextButtonDOM.innerHTML = "下一天";
+        resultBoxDOM.hidden = false;
+
+        setWeatherCondition();
+        setWeather();
+
+        setIncomePirce();
+        setNetPrice();
+
+        day++;
+
+        if (day % 5 == 0) {
+            moneyProfit -= 80;
+        }
+
+        if (weatherIsGood) {
+            moneyProfit += net.inshore * incomePrice.inshore;
+            moneyProfit += net.outshore * incomePrice.outshore;
+        } else {
+            moneyProfit += net.inshore * incomePrice.inshore * 2;
+
+            if (net.outshore > 0) {
+                netDamagedTextDOM.hidden = false;
+            }
+
+            net.outshore = 0;
+        }
+
+        moneyOwned += moneyProfit;
+
+        moneyProfitDOM.innerHTML = moneyProfit + '$';
+
+        if (moneyProfit < 0) {
+            moneyProfitDOM.className = "redText";
+        } else {
+            moneyProfitDOM.className = "greenText";
+        }
+
+        setStatusDOM();
+        setNetStatusDOM();
+        setIncomePriceDOM();
+    }
+}
+
+let netBuyNumberChange = {
+    increase() {
+        if ((netBuyNumber + 1) * netPrice <= moneyOwned) {
+            netBuyNumber++;
+            netBuyNumberDOM.innerHTML = netBuyNumber;
+
+            setNetBuyCost();
+        }
+    },
+    decrease() {
+        if (netBuyNumber > 0) {
+            netBuyNumber--;
+            netBuyNumberDOM.innerHTML = netBuyNumber;
+
+            setNetBuyCost();
+        }
+    }
+}
+
+function setNetBuyCost() {
+    moneyCostDOM.innerHTML = netBuyNumber * netPrice + '$';
+}
+
+function netBuy() {
+    netBuyNumberDOM.innerHTML = 0;
+
+    moneyOwned -= netBuyNumber * netPrice;
+
+    net.extra += netBuyNumber;
+
+    netBuyNumber = 0;
+
+    setNetBuyCost();
+    setStatusDOM();
+    setNetStatusDOM();
+}
+
+function netBuyAll () {
+    netBuyNumber = 0;
+
+    while (true) {
+        if ((netBuyNumber + 1) * netPrice < moneyOwned) {
+            netBuyNumber++;
+        } else {
+            break;
+        }
+    }
+
+    moneyOwned -= netBuyNumber * netPrice;
+
+    net.extra += netBuyNumber;
+
+    netBuyNumber = 0;
+
+    setStatusDOM();
+    setNetStatusDOM();
+}
+
+let netNumberChange = {
+    inshore: {
+        increase() {
+            if (net.extra > 0) {
+                net.inshore++;
+                net.extra--;
+
+                setNetStatusDOM();
+            }
+        },
+        decrease() {
+            if (net.inshore > 0) {
+                net.inshore--;
+                net.extra++;
+
+                setNetStatusDOM();
+            }
+        }
+    },
+    outshore: {
+        increase() {
+            if (net.extra > 0) {
+                net.outshore++;
+                net.extra--; 
+
+                setNetStatusDOM();
+            }
+        },
+        decrease() {
+            if (net.outshore > 0) {
+                net.outshore--;
+                net.extra++;
+
+                setNetStatusDOM();
+            }
+        }
     }
 }
 
@@ -145,4 +306,5 @@ setNetPrice();
 setStatusDOM();
 setNetStatusDOM();
 setIncomePriceDOM();
-setNextButtonDOM("start");
+
+netBuyNumberDOM.innerHTML = netBuyNumber;
